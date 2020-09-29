d7ec8e91242a9d9b3fee81d97eb036bd1502d7e8
sayhoChun <fishcreek@naver.com>
2020-09-29 18:18:09 +0900

    mysql conn added

---
+++ pkcDjango/models/user.py
@@ -0,0 +1,33 @@
+import json
+import django
+from django.db import models
+from django.utils import timezone
+from django.conf import settings
+from django.contrib.contenttypes.models import ContentType
+from django.utils.translation import ugettext_lazy as _, ugettext
+from django.urls.base import reverse
+from django.core.serializers.json import DjangoJSONEncoder
+from django.db.models.base import ModelBase
+
+from django.db.models.signals import post_migrate
+from django.contrib.auth.models import Permission
+
+import datetime
+import decimal
+
+
+class User(models.Model):
+    email = models.CharField(max_length=32)
+    password = models.CharField(max_length=32)
+    userType = models.IntegerField()
+    name = models.CharField(max_length=32)
+    nick = models.CharField(max_length=32)
+    sex = models.IntegerField()
+    bio = models.CharField(max_length=512)
+    career = models.TextField()
+    accessDate = models.DateTimeField()
+    regDate = models.DateTimeField()
+    status = models.IntegerField()
+
+    class Meta:
+        db_table = "tblUser"
---
+++ pkcDjango/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models.user import User
+
+
+def index(request):
+    list = User.objects.order_by('-regDate')[:20]
+    # output = ', '.join([str(item.id) for item in list])
+    context = {'list': list}
+    # return HttpResponse(output)
+    return render(request, "index.html", context)
+
+
+def detail(request, id):
+    return HttpResponse("test %s" % id)
+
